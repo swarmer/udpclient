eb086fb0930348f44f41f57232112d24bd977ab7
Anton Barkovsky <anton@swarmer.me>
2019-06-23 23:35:20 +0300

    Implement UDP listener

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::io;
 use std::io::Read;
+use std::net::{IpAddr, SocketAddr};
 
 use clap::{value_t, App, Arg, SubCommand};
+use futures::try_ready;
+use tokio;
+use tokio::net::UdpSocket;
+use tokio::prelude::*;
 
 
-pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 
 fn build_app() -> App<'static, 'static> {
@@ -27,17 +33,66 @@ fn build_app() -> App<'static, 'static> {
                         .help("Target port"),
                 ),
         )
+        .subcommand(
+            SubCommand::with_name("listen")
+                .about("Listen to UDP packets")
+                .arg(
+                    Arg::with_name("PORT")
+                        .required(true)
+                        .index(1)
+                        .help("Port on which to receive packets"),
+                ),
+        )
 }
 
 
 #[derive(Clone, Debug)]
-pub struct SendArgs {
+struct ListenArgs {
+    pub port: u16,
+}
+
+
+struct UdpListenerFuture {
+    socket: UdpSocket,
+}
+
+impl Future for UdpListenerFuture {
+    type Item = ();
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<(), io::Error> {
+        loop {
+            let mut buf = vec![0; 65536];
+            let (size, addr) = try_ready!(self.socket.poll_recv_from(&mut buf));
+
+            eprintln!("Received a packet from {}", addr);
+            let packet_str = String::from_utf8_lossy(&buf[..size]);
+            eprintln!("UTF-8 representation: {:?}", packet_str);
+            eprintln!("Byte array representation: {:?}", &buf[..size]);
+            eprintln!();
+        }
+    }
+}
+
+
+fn cli_listen(args: &ListenArgs) {
+    let addr = SocketAddr::from(("0.0.0.0".parse::<IpAddr>().unwrap(), args.port));
+    let socket = UdpSocket::bind(&addr).expect("Failed to open a UDP socket");
+    eprintln!("Listening on port {}...", args.port);
+
+    let listener_future = UdpListenerFuture { socket };
+    tokio::run(listener_future.map_err(|e| eprintln!("Error: {:?}", e)));
+}
+
+
+#[derive(Clone, Debug)]
+struct SendArgs {
     pub host: String,
     pub port: u16,
 }
 
 
-pub fn cli_send(args: &SendArgs) {
+fn cli_send(args: &SendArgs) {
     let mut buf = vec![];
     std::io::stdin()
         .read_to_end(&mut buf)
@@ -51,7 +106,7 @@ pub fn cli_send(args: &SendArgs) {
 }
 
 
-pub fn run() -> i32 {
+fn run() -> i32 {
     let mut app = build_app();
     let matches = app.clone().get_matches();
 
@@ -63,12 +118,26 @@ pub fn run() -> i32 {
                 port: match value_t!(submatches, "PORT", u16) {
                     Ok(port) => port,
                     Err(e) => {
-                        println!("{}", e.message);
+                        eprintln!("{}", e.message);
                         return 1;
                     }
                 },
             };
             cli_send(&send_args);
+            0
+        }
+        Some("listen") => {
+            let submatches = matches.subcommand_matches("listen").unwrap();
+            let listen_args = ListenArgs {
+                port: match value_t!(submatches, "PORT", u16) {
+                    Ok(port) => port,
+                    Err(e) => {
+                        eprintln!("{}", e.message);
+                        return 1;
+                    }
+                },
+            };
+            cli_listen(&listen_args);
             0
         }
         None => {
